e497b6409b235a14087d4011ac255fddbc726200
HarshitKumarSahu <poltuharshit14@gmail.com>
2025-10-24 18:28:12 +0530

    added phrases.js

--- src/js/initPhaseThree.js
+++ src/js/initPhaseThree.js
@@ -1,7 +1,5 @@
 import getStream from "./getStream.js"
-
-
-
+import setNextPhrase from './setNextPhrase'
 
 // init phase 03
 export default async () => {
@@ -12,4 +10,21 @@ export default async () => {
     // hide phase 02 and show phase 03
     document.body.classList.remove("phase-02-active")
     document.body.classList.add("phase-03-active")
+
+    const streamW = stream.getVideoTracks()[0].getSettings().width
+    const streamH = stream.getVideoTracks()[0].getSettings().height
+
+    // show video
+    var video = document.querySelector("video.face-readout")
+    video.dataset.streamWidth = streamW
+    video.dataset.streamHeight = streamH
+    video.srcObject = stream
+    video.onloadedmetadata = async () => {
+        video.play()
+    }
+
+    // Display initial phrase
+    // for user to speak
+    setNextPhrase()
+    
 }
